58d38abe13839b2094806bee6f3ee9b0250e7772
danila_tsarev <danila_tsarev@epam.com>
2022-05-24 19:20:31 +0300

    requirements.txt added

--- settings_gui.py
+++ settings_gui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\Pictures\Projects\Python\epam_progects\price_checker_gui\checker.ui'
+
 #
 # Created by: PyQt5 UI code generator 5.15.2
 #
